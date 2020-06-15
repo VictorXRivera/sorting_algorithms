b79cd6370584183dddfebbf5dd9558fe6db79d90
VictorXRivera <victorxrivera.professional@gmail.com>
2020-06-15 14:25:46 +0000

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * swap_2 - swaping elements of array
+ * @xp: element
+ * @yp: element
+ * Return: Void
+ */
+void swap_2(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+/**
+ * selection_sort - selection sort
+ * @array: array
+ * @size: size of array
+ * Return: Void
+ */
+void selection_sort(int *array, size_t size)
+{
+	unsigned int h;
+	unsigned int b;
+	unsigned int min_idx;
+
+	for (h = 0; h < size; h++)
+	{
+		min_idx = h;
+		for (b = h + 1; b < size; b++)
+		{
+			if (array[b] < array[min_idx])
+			{
+				min_idx = b;
+				swap_2(&array[min_idx], &array[h]);
+				print_array(array, size);
+			}
+		}
+	}
+}
